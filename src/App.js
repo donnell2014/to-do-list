f6b1f1ec9fd0f92e95f20389e20f9579f187c2be
Donnell Albertie <dalbertie5@gmail.com>
2021-07-17 23:01:26 -0500

    ToDoList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [newToDo, setNewToDo] = useState("");
+  const [todos, setToDos] = useState([]);
+
+  const handleNewToDoSubmit = (event) => {
+    event.preventDefault();
+
+    if(newToDo.length == 0) {
+      return;
+    }
+
+    const todoItem = {
+      text: newToDo,
+      complete: false
+    }
+
+
+    setToDos([...todos, todoItem]);
+    setNewToDo("");
+  };
+
+  const handleToDoDelete = (delIdx) => {
+    const filteredTodos = todos.filter((todo, i) => {
+      return i != delIdx;
+    });
+    
+    setToDos(filteredTodos);
+  }
+
+  const handleToggleComplete = (idx) => {
+    const updatedTodos = todos.map((todo, i) => {
+      if(idx === i) {
+        todo.complete = !todo.complete;
+        // const updatedTodo = { ...todo, complete: !todo.complete };
+        // return updatedTodo
+      }
+
+
+      return todo;
+    });
+
+    setToDos(updatedTodos);
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(event) => {
+        handleNewToDoSubmit(event);
+      }}>
+        <input onChange={(event) => {
+          setNewToDo(event.target.value);
+        }} type="text" value={newToDo}/>
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+
+      {todos.map((todo, i) => {
+        return (
+          <div key="i">
+            <input onChange={(event) => {
+              handleToggleComplete(i);
+            }} checked={todo.complete} type="checkbox"></input>
+            <span>{todo.text}</span>
+            <button onClick={(event) => {
+              handleToDoDelete(i);
+            }}>Delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
